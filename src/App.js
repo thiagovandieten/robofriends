04c64648611c8a400331355a982423584179c721
Thiago van Dieten <thiago@tvdieten.com>
2024-02-01 08:04:09 +0100

    Fetching users from typicode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React, {Component, useState} from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './Searchbox';
 import './App.css';
-import {robots} from './robots.js';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots,
+            robots: [],
             searchField: ''
         }
     }
@@ -21,15 +20,22 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         })
-        this.state.robots = filteredRobots;
         
         return (
             <div className="tc">
                 <h1 className="f1">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={this.state.robots}/>
+                <CardList robots={filteredRobots}/>
             </div>
         )
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users', {
+            method: 'get'
+        })
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import CardList from './CardList';
-import {robots} from './robots.js';
 // import reportWebVitals from './reportWebVitalgis';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App robots={robots} />
+    <App />
   </React.StrictMode>
 );
 
