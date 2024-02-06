3bd21f225856fd783aba2a86508d22ffb2f784cf
Thiago van Dieten <thiago@tvdieten.com>
2024-02-06 03:46:50 +0100

    turn App into a functional comp

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,63 +1,56 @@
 "use client";
 
-import React, {Component} from 'react';
-import CardList from '../components/CardList';
-import SearchBox from '../components/Searchbox';
-import Scroll from '../components/Scroll';
-import { ErrorBoundary } from 'react-error-boundary';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/Searchbox";
+import Scroll from "../components/Scroll";
+import {ErrorBoundary} from "react-error-boundary";
+import "./App.css";
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchField: ''
-        }
-    }
+const App = () => {
+    // Declare states
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState('');
 
-    fallbackRender  = ({ error, resetErrorBoundary }) => (
-        <div role="alert">
-            <p>Something went wrong:</p>
-            <pre>{error.message}</pre>
-        </div>
-    );
-    
-    onSearchChange = (event) => {
-        this.setState({searchField: event.target.value});
-    }
-    
-    render() {
-        const {robots, searchField} = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchField.toLowerCase());
-        })
-
-        if (!robots.length) {
-            return <h1 className="tc">Loading...</h1>
-        }
-        else {
-            return (
-                <div className="tc">
-                    <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
-                        <ErrorBoundary FallbackComponent={this.fallbackRender}>
-                            <CardList robots={filteredRobots}/>
-                        </ErrorBoundary>
-                    </Scroll>
-                </div>
-            )
-        }
-        
-    }
-
-    componentDidMount() {
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users', {
             method: 'get'
         })
         .then(response => response.json())
-        .then(users => this.setState({robots: users}));
+        .then(users => setRobots(users));
+    }) 
+    
+    const fallbackRender = ({error, resetErrorBoundary}) => {
+        <div role="alert">
+            <p>Something went wrong</p>
+            <pre>{error.message}</pre>
+        </div>
+    }
+
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value);
+    }
+
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase())
+    })
+
+    if(!robots.length) {
+        return <h1 className="tc">Loading...</h1>
+    }
+
+    else {
+        return (
+            <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={onSearchChange} />
+                    <Scroll>
+                        <ErrorBoundary FallbackComponent={fallbackRender}>
+                            <CardList robots={filteredRobots}/>
+                        </ErrorBoundary>
+                    </Scroll>
+                </div>
+        )
     }
 }
 
